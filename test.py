97097930905bf884d50eea2be00b5a456496da7e
Lawrence Abdulnour <Lawrenceee94@gmail.com>
2019-03-25 20:42:19 -0400

    testing

--- test.py
+++ test.py
@@ -418,11 +418,12 @@ def run_epoch(model, data, is_train=False, lr=1.0):
         loss_T.backward()
         grads_norm = []
 
-        means = torch.zeros([model.batch_size, model.emb_size])
+        means = torch.zeros([model.batch_size, model.hidden_size])
         pdb.set_trace()
         for t in range(model.seq_len):
              for l in range(model.num_layers):
                  means = torch.add(means,hidden_timesteps[t][l].grad)
+             means = means / (model.num_layers)
              norm = torch.norm(means)
              grads_norm.append(norm)
 
